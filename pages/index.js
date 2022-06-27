ae15b2b9c2291f677f1be099690c1eae9b1ebb33
Uzair Qidwai <uzairqidwai1@gmail.com>
2022-06-26 20:33:22 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -227,14 +227,19 @@ export default function Home() {
                     <h3 className="text-xl font-display font-medium">Coming Soon!</h3>
                     <p className="font-display mt-1 text-gray-500">Great things coming up</p>
                 </a>
+                  </div>
+                  </div>
+                </a>
 
-              <div className="grid grid-cols-2  gap-5 mt-9 max-w-6xl">
+              <div className="grid grid-cols-1  gap-5 mt-9 max-w-6xl">
                 <a href="https://www.gocybersense.com" target="none" className="bg-white rounded-lg flex flex-col shadow-md">
                   <div className="h-48 w-full overflow-hidden">
                     <img className="object-cover rounded-t-lg w-auto" src="/icons/c-logo.svg" />
                   </div>
-                  </div>
-              </div>
+                  <div className="flex-shrink-1 px-5 pb-5 pt-3">
+                    <h3 className="text-xl font-display font-medium">Coming Soon!</h3>
+                    <p className="font-display mt-1 text-gray-500">Great things coming up</p>
+                </a>
                   </div>
                   </div>
                 </a>
